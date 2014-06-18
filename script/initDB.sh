2ae2671ecca097cd2cfa12b28acbf9a3fcb7b6aa
Chris Cheung <chris@aresgmaes.com.hk>
2014-06-18 11:11:26 +0800

    fix ruby install

--- script/initDB.sh
+++ script/initDB.sh
@@ -1,4 +1,5 @@
 cd /opt/redmine
+bundle install --without development test
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake redmine:load_default_data
---
+++ script/installRuby.sh
@@ -0,0 +1,3 @@
+curl -sSL https://get.rvm.io | bash -s stable --ruby=2.0.0
+source ~/.rvm/scripts/rvm
+rvm use ruby-2.0 --create
