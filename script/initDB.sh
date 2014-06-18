93fa48a0bfa65853e92d18633222fbe4be219380
Chris Cheung <chris@aresgmaes.com.hk>
2014-06-18 11:34:54 +0800

    provide answer

--- script/initDB.sh
+++ script/initDB.sh
@@ -2,4 +2,6 @@ cd /opt/redmine
 bundle install --without development test
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake redmine:load_default_data
+
+echo "en" > ans
+RAILS_ENV=production bundle exec rake redmine:load_default_data < ans
